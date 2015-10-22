25d7c2365deece31b7033d3a0fffd4f3fde7cc52
KatherineP <gucolk@gmail.com>
2015-10-22 14:03:18 +0300

    first commit

---
+++ src/main/java/frameWork/HelpersBase.java
@@ -0,0 +1,13 @@
+package frameWork;
+
+/**
+ * Created by Alex on 21.10.2015.
+ */
+public class HelpersBase {
+
+    protected final ApplicationManager manager;
+
+    public HelpersBase(ApplicationManager applicationManager){
+        this.manager = applicationManager;
+    }
+}
